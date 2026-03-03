b6dfcc28b3e5b5b4851e64b645291e28f9c7b6bb
lmthao <32890277+dzodzanh@users.noreply.github.com>
2026-03-03 12:23:41 +0700

    Add files via upload

---
+++ QuizTest/quiz/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.utils import timezone
+
+class Exam(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+class Question(models.Model):
+    exam = models.ForeignKey(Exam, related_name='questions', on_delete=models.CASCADE)
+    text = models.TextField()
+    order = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ['order', 'id']
+
+    def __str__(self):
+        return f"{self.exam.title} - {self.text[:50]}"
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, related_name='choices', on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
